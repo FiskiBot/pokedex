4f80fe86ee23e206cff3f61c6d9d79c2fc568f00
Fiski <patrick.moening@patrickacofdoom.bigfish.lan>
2016-04-11 10:57:25 -0700

    End of lesson, Configured part of the model

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,23 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Patrick Moening on 4/11/16.
+//  Copyright © 2016 BigFiski. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    var name: String{return _name}
+    private var _pokedexId: Int!
+    var pokedexId: Int {return _pokedexId}
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
