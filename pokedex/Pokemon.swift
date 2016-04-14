dab9e9e814602f5b7fd07f409882de87ad0cc2ee
Fiski <patrick.moening@patrickacofdoom.bigfish.lan>
2016-04-14 16:08:00 -0700

    Set outlets and the references in Pokemon class. Lesson 11 complete

--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -10,10 +10,29 @@ import Foundation
 
 class Pokemon {
     private var _name: String!
-    var name: String{return _name}
+    var name: String {return _name}
     
     private var _pokedexId: Int!
     var pokedexId: Int {return _pokedexId}
+    
+    private var _type: String!
+    var type: String {return _type}
+    
+    private var _height: String!
+    var height: String {return _height}
+    
+    private var _weight: String!
+    var weight: String {return _weight}
+    
+    private var _attack: String!
+    var attack: String {return _attack}
+    
+    private var _defense: String!
+    var defense: String {return _defense}
+    
+    private var _nextEvo: String!
+    var nextEvo: String {return _nextEvo}
+    
     
     init(name: String, pokedexId: Int){
         self._name = name
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -13,6 +13,21 @@ class PokemonDetailVC: UIViewController {
     var pokemon : Pokemon!
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var mainDescription: UILabel!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var idLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    
     
     override func viewDidAppear(animated: Bool) {
         nameLbl.text = pokemon.name
